0338355b5e3be8639529e08ce606933926ac80ff
Guerracarlos1309 <guerracarlos1309@gmail.com>
2025-02-02 12:38:06 -0400

    cambio

--- src/api/helpFetch.js
+++ src/api/helpFetch.js
@@ -1,5 +1,5 @@
 export const helpFetch = () => {
-  const URL = 'https://json-dise-o-1.onrender.com'
+  const URL = 'https://json-dise-o-2.onrender.com'
 
   const customFetch = (endpoint, options = {}) => {
     options.method = options.method || 'GET'
